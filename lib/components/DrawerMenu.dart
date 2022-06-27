9a1c69fcec194dcd98c77a53585d5ca51c5889b3
Dominique Kanyik <domykanyiktesh01@gmail.com>
2022-06-27 02:06:54 +0200

    Nej 1.4.7
    - Few adjustments.

--- lib/components/DrawerMenu.dart
+++ lib/components/DrawerMenu.dart
@@ -158,7 +158,7 @@ class _DrawerMenuState extends State<DrawerMenu> {
                             },
                             child: const Text('Legal',
                                 style: TextStyle(fontSize: 16))),
-                        trailing: const Text('v1.4.6',
+                        trailing: const Text('v1.4.7',
                             style: TextStyle(fontSize: 16, color: Colors.grey)),
                       ),
                     ))),
