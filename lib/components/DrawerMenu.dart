660af450459f6d790921c7c5b5d83f97d4f49735
Dominique <dominique@kedokagroup.com>
2023-10-30 16:02:26 +0200

    Version back to 1

--- lib/components/DrawerMenu.dart
+++ lib/components/DrawerMenu.dart
@@ -145,7 +145,7 @@ class _DrawerMenuState extends State<DrawerMenu> {
                             },
                             child: Text('drawer.legal'.tr(),
                                 style: TextStyle(fontSize: 16))),
-                        trailing: const Text('v1.7.5',
+                        trailing: const Text('v1.0.0',
                             style: TextStyle(fontSize: 16, color: Colors.grey)),
                       ),
                     ))),
