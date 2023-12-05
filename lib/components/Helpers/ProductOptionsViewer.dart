0b4fdb426dd6749617a3d836437e49c656107b95
Dominique <dominique@kedokagroup.com>
2023-12-05 03:04:05 +0200

    options wip

---
+++ lib/components/Helpers/ProductOptionsViewer.dart
@@ -0,0 +1,145 @@
+import 'package:dulcetdash/components/Helpers/DataParser.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class ProductOptionsViewer extends StatefulWidget {
+  final Map productData;
+
+  const ProductOptionsViewer({super.key, required this.productData});
+
+  @override
+  State<ProductOptionsViewer> createState() =>
+      _ProductOptionsViewerState(productData: productData);
+}
+
+class _ProductOptionsViewerState extends State<ProductOptionsViewer> {
+  final Map productData;
+  Map<String, dynamic> pizzaSelectedOptions = {};
+  final List<String> allowedKeys = ['size', 'base', 'cheese'];
+  DataParser dataParser = DataParser();
+
+  _ProductOptionsViewerState({required this.productData});
+
+  void _pizzaAutoSelectOptions() {
+    if (productData['options'].runtimeType == Map) {
+      return;
+    }
+    // for (String key in allowedKeys) {
+    //   if (productData['options'][key] != null &&
+    //       productData['options'][key].isNotEmpty) {
+    //     // Auto-select the first option for each key
+    //     pizzaSelectedOptions[key] = productData['options'][key][0];
+    //   }
+    // }
+
+    // Additional auto-selection based on specific criteria
+    _autoSelectOptionByName('size', 'standard pizza (23cm)');
+    _autoSelectOptionByName('base', 'original');
+    _autoSelectOptionByName('cheese', 'normal cheese');
+
+    setState(() {});
+  }
+
+  void _autoSelectOptionByName(String key, String optionName) {
+    final options = productData['options'][key];
+    if (options != null) {
+      final autoSelectedOption = options.firstWhere(
+        (option) => option['name'] == optionName,
+        orElse: () => null,
+      );
+
+      if (autoSelectedOption != null) {
+        pizzaSelectedOptions[key] = autoSelectedOption;
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _pizzaAutoSelectOptions();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.blue, height: 120, child: getFirstLineOptions());
+  }
+
+  Widget getFirstLineOptions() {
+    //Check if the options are for a pizza or other products
+    var isPizza = productData['options'].containsKey('size');
+    var productOptions = productData['options'];
+
+    if (isPizza) {
+      final Map<String, dynamic> filteredMap = Map.fromIterable(
+        productOptions.keys.where((key) => allowedKeys.contains(key)),
+        key: (key) => key,
+        value: (key) => productOptions[key],
+      );
+
+      print(pizzaSelectedOptions);
+
+      return Container(
+        color: Colors.red,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            for (String key in allowedKeys)
+              if (pizzaSelectedOptions.containsKey(key))
+                _buildOptionRow(key, pizzaSelectedOptions[key]),
+            TextButton(
+                onPressed: () {
+                  // Show the modal to change options
+                  print('Show modal');
+                },
+                child: Text('Change')),
+          ],
+        ),
+      );
+    } else {
+      return Row(
+        children: [
+          Text(
+            'Tamanho: ',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text(productData['options']['name'].toString()),
+          Text(
+            ' - R\$ ${productData['options']['price'].toString()}',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ],
+      );
+    }
+  }
+
+  Widget _buildOptionRow(String mainKey, Map<String, dynamic> option) {
+    return Flexible(
+      flex: 1,
+      child: Container(
+        color: Colors.green,
+        width: MediaQuery.of(context).size.width /
+            (pizzaSelectedOptions.keys.length * 1.5),
+        child: Column(
+          children: [
+            Text(
+              '${dataParser.capitalizeWords(mainKey)}',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(
+              dataParser.capitalizeWords(option['name'].toString()),
+              textAlign: TextAlign.center,
+            ),
+            Expanded(child: SizedBox.shrink()),
+            Text(
+              'N\$ ${option['price'].toString()}',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/components/Shopping/ProductView.dart
+++ lib/components/Shopping/ProductView.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:developer';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dulcetdash/components/Helpers/ProductOptionsViewer.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hexcolor/hexcolor.dart';
@@ -30,53 +31,6 @@ class _ProductViewState extends State<ProductView> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    //! Get the stores names
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      GetCatalogueL1(context: context);
-    });
-  }
-
-  Future GetCatalogueL1({required BuildContext context}) async {
-    //....
-    // Uri mainUrl = Uri.parse(Uri.encodeFull(
-    //     '${context.read<HomeProvider>().bridge}/getCatalogueFor'));
-
-    // //Assemble the bundle data
-    // //* @param type: the type of request (past, scheduled, business)
-    // Map<String, String> bundleData = {
-    //   'store': context.read<HomeProvider>().selected_store['store_fp'],
-    //   'structured': 'true'
-    // };
-
-    // try {
-    //   http.Response response = await http.post(mainUrl, body: bundleData);
-
-    //   if (response.statusCode == 200) //Got some results
-    //   {
-    //     // log(response.body.toString());
-    //     Map tmpResponse = json.decode(response.body);
-    //     // log(tmpResponse['response']['WOMEN'].toString());
-    //     //? Update
-    //     context
-    //         .read<HomeProvider>()
-    //         .updateCatalogueLevel1_structured(data: tmpResponse['response']);
-    //     setState(() {
-    //       isLoading = false;
-    //     });
-    //   } else //Has some errors
-    //   {
-    //     log(response.toString());
-    //     Timer(const Duration(milliseconds: 500), () {
-    //       GetCatalogueL1(context: context);
-    //     });
-    //   }
-    // } catch (e) {
-    //   log('8');
-    //   log(e.toString());
-    //   Timer(const Duration(milliseconds: 500), () {
-    //     GetCatalogueL1(context: context);
-    //   });
-    // }
   }
 
   @override
@@ -178,6 +132,7 @@ class ShowProductMain extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
         child: ProductDisplayModel(
+          productData: productData,
           productImage: productData['pictures'][0].runtimeType.toString() ==
                   'List<dynamic>'
               ? productData['pictures'][0][0]
@@ -195,9 +150,11 @@ class ProductDisplayModel extends StatelessWidget {
   final String productImage;
   final String productName;
   final String productPrice;
+  final Map productData;
 
   const ProductDisplayModel(
       {Key? key,
+      required this.productData,
       required this.productImage,
       required this.productName,
       required this.productPrice})
@@ -206,13 +163,12 @@ class ProductDisplayModel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      // color: Colors.red,
       child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
         Padding(
           padding: const EdgeInsets.only(bottom: 35),
           child: SizedBox(
             width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.26,
+            height: MediaQuery.of(context).size.height * 0.24,
             child: CachedNetworkImage(
               fit: BoxFit.contain,
               imageUrl: productImage,
@@ -245,6 +201,22 @@ class ProductDisplayModel extends StatelessWidget {
               maxLines: 5,
               style: TextStyle(fontFamily: 'MoveTextBold', fontSize: 19)),
         ),
+        Visibility(
+          visible: productData['description'] != null &&
+              productData['description'].toString().trim().length > 0,
+          child: SizedBox(
+            height: 10,
+          ),
+        ),
+        Visibility(
+          visible: productData['description'] != null &&
+              productData['description'].toString().trim().length > 0,
+          child: Text(productData['description'].toString(),
+              style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: 'MoveText',
+                  color: AppTheme().getGenericDarkGrey())),
+        ),
         SizedBox(
           height: 25,
         ),
@@ -268,6 +240,10 @@ class ProductDisplayModel extends StatelessWidget {
                     fontSize: 21,
                     color: AppTheme().getPrimaryColor())),
           ],
+        ),
+        Divider(),
+        ProductOptionsViewer(
+          productData: productData,
         ),
         Expanded(child: Text('')),
         Visibility(
