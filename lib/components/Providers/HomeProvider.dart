1e1bdc97c14e4c462d9751cc6b3eaaf5a3e3fd0d
Dominique <dominique@kedokagroup.com>
2023-12-14 14:26:29 +0200

    Debug mode

--- lib/components/Providers/HomeProvider.dart
+++ lib/components/Providers/HomeProvider.dart
@@ -19,7 +19,7 @@ import 'package:collection/collection.dart';
 // Will hold all the home related globals - only!
 
 class HomeProvider with ChangeNotifier {
-  final String bridge = 'http://172.20.10.2:9697';
+  final String bridge = 'http://192.168.178.93:9697';
   // final String bridge = 'https://api.dulcetdash.com';
 
   String selectedService =
@@ -1644,6 +1644,9 @@ class HomeProvider with ChangeNotifier {
 
   List getUnifiedArrayOfGenericOptions(
       {required Map<String, dynamic> product}) {
+    if (globalSelectedOptions[product['id']] == null) {
+      return product['options'];
+    }
     // Create a set to store unique values
     Set unifiedSet = {};
 
